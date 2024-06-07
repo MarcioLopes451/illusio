f12f97a1c3f60c0c326f9b85da06458de9a8f8b9
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-06-07 23:37:06 +0100

    add: final changes for now

--- src/components/Navbar/MobileNav.tsx
+++ src/components/Navbar/MobileNav.tsx
@@ -12,7 +12,7 @@ export default function MobileNav({ isOpen, onClose }: Nav) {
   };
 
   return isOpen ? (
-    <div className="fixed top-24 bg-transparent z-[50] font-Roboto w-full">
+    <div className="fixed top-24 bg-transparent z-50 font-Roboto w-full">
       <div className="bg-black py-9 h-screen">
         <div className="flex mt-20 flex-col gap-9 text-white text-2xl px-6 font-light tracking-widest text-center">
           <Link to="/" onClick={scrollUp}>
